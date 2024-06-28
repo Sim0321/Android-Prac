cf3a958ee422fd89a3b5d3d9af42b3fe4b451848
Sim <0doogadooga@gmail.com>
2024-06-28 13:28:51 +0900

    intent로 webview

--- webviewtest/src/main/java/com/example/webview/MainActivity.kt
+++ webviewtest/src/main/java/com/example/webview/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.webview
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doAfterTextChanged
-import androidx.core.widget.doBeforeTextChanged
-import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +19,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val webview = findViewById<WebView>(R.id.webview)
-        val urlPrefix = "httpS://"
+        webview.setWebViewClient(object : WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+        })
+
+        try {
+            webview.loadUrl(
+                intent.data!!.toString()
+            )
+        } catch (exception:Exception){}
+
+        val urlPrefix = "http://"
         var finalUrl=""
 
         val address = findViewById<EditText>(R.id.address)
@@ -26,28 +42,30 @@ class MainActivity : AppCompatActivity() {
         // addTextChangedListener 사용법
         address.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                Log.d("testt", "beforeTextChanged : " + s)
+//                Log.d("testt", "beforeTextChanged : " + s)
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d("testt", "onTextChanged : " + s)
+//                Log.d("testt", "onTextChanged : " + s)
             }
 
             override fun afterTextChanged(s: Editable?) {
                 finalUrl = urlPrefix + s.toString()
-                Log.d("testt", "afterTextChanged : " + s)
+//                Log.d("testt", "afterTextChanged : " + s)
             }
         })
 
         // addTextChangedListener 람다 사용법
-        address.doBeforeTextChanged { text, start, count, after ->  }
-        address.doOnTextChanged { text, start, before, count ->  }
-        address.doAfterTextChanged {  }
+//        address.doBeforeTextChanged { text, start, count, after ->  }
+//        address.doOnTextChanged { text, start, before, count ->  }
+//        address.doAfterTextChanged {  }
 
         val open = findViewById<TextView>(R.id.open)
         open.setOnClickListener {
             val url = address.text.toString()
-            webview.loadUrl(finalUrl)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            Log.d("testt", "잘됨")
+            startActivity(intent)
         }
     }
 }
